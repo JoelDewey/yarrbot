5ca836bc921db48ac72e212f481432b842606734
Joel Dewey <hello@joeldewey.com>
2021-08-26 21:42:07 -0400

    The bot now automatically joins rooms.
    The bot should also reject invites from unauthorized users.

--- crates/matrix_client/src/command_parser.rs
+++ crates/matrix_client/src/command_parser.rs
@@ -6,16 +6,33 @@ use anyhow::Result;
 use matrix_sdk::{
     async_trait,
     events::{
+        room::member::MemberEventContent,
         room::message::{MessageEventContent, MessageType, TextMessageEventContent},
-        AnyMessageEventContent, SyncMessageEvent,
+        AnyMessageEventContent, StrippedStateEvent, SyncMessageEvent,
     },
     room::Room,
     Client, EventHandler,
 };
+use rand::distributions::Uniform;
+use rand::prelude::*;
 use std::collections::VecDeque;
+use tokio::time::{sleep, Duration};
 use yarrbot_db::DbPool;
+use yarrbot_db::{actions::user_actions::UserActions, models::User};
 
 const YARRBOT_COMMAND: &str = "!yarrbot";
+
+fn user_exists(pool: &DbPool, username: &str) -> Result<bool> {
+    let conn = pool.get()?;
+    let u = User::try_get_by_username(&conn, username)?;
+    match u {
+        Some(u) => {
+            debug!("User \"{}\" exists with ID {}.", username, u.id.to_string());
+            Ok(true)
+        }
+        None => Ok(false),
+    }
+}
 
 pub struct CommandMetadata {
     pub user: String,
@@ -97,6 +114,89 @@ impl EventHandler for CommandParser {
                     error!("Encountered error while responding to command: {:?}", e);
                 }
             }
+        }
+    }
+
+    async fn on_stripped_state_member(
+        &self,
+        room: Room,
+        room_member: &StrippedStateEvent<MemberEventContent>,
+        _: Option<MemberEventContent>,
+    ) {
+        // Based off of https://github.com/matrix-org/matrix-rust-sdk/blob/0.3.0/matrix_sdk/examples/autojoin.rs
+
+        // Don't respond to invites if they're not meant for us.
+        if room_member.state_key != self.client.user_id().await.unwrap() {
+            return;
+        }
+
+        if let Room::Invited(room) = room {
+            // Synapse has a bug where the bot can receive an invite, but the server isn't ready
+            // for the bot to join the room.
+            // https://github.com/matrix-org/synapse/issues/4345
+
+            // Don't let users that aren't admins invite the bot to rooms.
+            let room_name = room
+                .display_name()
+                .await
+                .unwrap_or_else(|_| String::from("(Unknown Name)"));
+            let room_id = room.room_id().as_str();
+            let username = room_member.sender.as_str();
+            match user_exists(&self.pool, username) {
+                Ok(exists) => {
+                    if !exists {
+                        match room.reject_invitation().await {
+                            Ok(_) => {
+                                warn!("\"{}\" attempted to invite the bot to room \"{}\" ({}) but is not authorized to do so.", username, &room_name, room_id);
+                            }
+                            Err(e) => {
+                                error!("Failed to reject room invitation from \"{}\" ({}) for room \"{}\": {:?}", username, &room_name, room_id, e);
+                            }
+                        };
+                        return;
+                    }
+                }
+                Err(e) => {
+                    error!(
+                        "Error encountered while checking if inviting user exists: {:?}",
+                        e
+                    );
+                    return;
+                }
+            };
+
+            let mut last_error: Option<matrix_sdk::Error> = None;
+            let mut rng: SmallRng = SmallRng::from_entropy();
+            let dist = Uniform::new_inclusive(0, 1000);
+            for i in 0..5 {
+                match room.accept_invitation().await {
+                    Ok(_) => {
+                        info!(
+                            "Joined room \"{}\" ({}) after invitation from \"{}\".",
+                            &room_name, room_id, username
+                        );
+                        return;
+                    }
+                    Err(e) => {
+                        let base: u64 = u64::pow(2, i) * 100;
+                        let jitter: u64 = dist.sample(&mut rng);
+                        let delay = base + jitter;
+                        debug!(
+                            "Encountered error while attempting to join room \"{}\" ({}), delaying for {} ms: {:?}",
+                            room_id, &room_name, delay, e
+                        );
+                        sleep(Duration::from_millis(delay)).await;
+                        last_error = Some(e);
+                    }
+                }
+            }
+
+            error!(
+                "Failed to join room \"{}\" ({}) after five attempts; last error encountered: {:?}",
+                room_id,
+                &room_name,
+                last_error.unwrap()
+            );
         }
     }
 }

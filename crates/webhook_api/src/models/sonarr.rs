c00e105cac1b69bab9784ab6c9102c339e3f2dd9
Joel Dewey <hello@joeldewey.com>
2021-08-28 12:04:20 -0400

    Missing some properties on this model.

--- crates/webhook_api/src/models/sonarr.rs
+++ crates/webhook_api/src/models/sonarr.rs
@@ -63,6 +63,8 @@ pub struct SonarrEpisodeFile {
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "camelCase")]
 pub struct SonarrRenamedEpisodeFile {
+    pub relative_path: Option<String>,
+    pub path: Option<String>,
     pub previous_relative_path: Option<String>,
     pub previous_path: Option<String>,
 }

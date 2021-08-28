d48d318092aeded0d045973e2f5b0d7673180770
Joel Dewey <hello@joeldewey.com>
2021-08-28 10:27:23 -0400

    Added the ability to have section heading elements in the message_data_builder.rs

--- crates/matrix_client/src/message/message_data_builder.rs
+++ crates/matrix_client/src/message/message_data_builder.rs
@@ -1,8 +1,9 @@
 //! Utilities for building [MessageData] structs for delivery of Matrix messages.
 
 use crate::message::MessageData;
+use std::fmt::Write;
 
-const DEFAULT_PLAIN_BREAK: &str = "|";
+const DEFAULT_PLAIN_BREAK: &str = "\n";
 const DEFAULT_HTML_BREAK: &str = "<br>";
 
 /// Implementors may implement this trait to allow a struct to format its data for display in Matrix.
@@ -23,6 +24,17 @@ pub trait MatrixMessageDataPart {
     fn to_html(&self, break_character: &str) -> String;
 }
 
+/// Represents the level of section heading; otherwise known as `<h1>` through `<h6>`.
+#[allow(dead_code)]
+pub enum SectionHeadingLevel {
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+}
+
 /// Builds [MessageData] structs with special formatting. Forward-only.
 pub struct MessageDataBuilder {
     plain_parts: String,
@@ -30,23 +42,22 @@ pub struct MessageDataBuilder {
 }
 
 impl MessageDataBuilder {
-    pub(crate) fn new() -> Self {
+    pub fn new() -> Self {
         MessageDataBuilder {
             plain_parts: String::new(),
-            html_parts: String::new(),
+            html_parts: String::from(DEFAULT_HTML_BREAK),
         }
     }
 
     /// Adds some character or set of characters that "break" or "separate" message data parts.
     /// For HTML text messages, a line break (`<br>`) after the last part added to the builder.
-    /// For plain text messages, the default separator `|` is added instead.
-    pub(crate) fn break_character(mut self) -> Self {
+    /// For plain text messages, the default separator `\n` is added instead.
+    pub fn break_character(&mut self) {
         self.plain_parts.push(' ');
         self.plain_parts.push_str(DEFAULT_PLAIN_BREAK);
 
         self.html_parts.push(' ');
         self.html_parts.push_str(DEFAULT_HTML_BREAK);
-        self
     }
 
     /// Adds a key-value data pair to the message with a line break (or the default plain text separator).
@@ -54,10 +65,10 @@ impl MessageDataBuilder {
     /// # Examples
     /// Plain: `key: value`
     /// Rich: `<strong>key</strong>: value`
-    pub(crate) fn add_key_value(mut self, key: &str, value: &str) -> Self {
-        self.plain_parts.push(' ');
+    pub fn add_key_value(&mut self, key: &str, value: &str) {
+        self.plain_parts.push_str(" **");
         self.plain_parts.push_str(key);
-        self.plain_parts.push_str(": ");
+        self.plain_parts.push_str("**: ");
         self.plain_parts.push_str(value);
 
         self.html_parts.push_str("<strong>");
@@ -65,7 +76,7 @@ impl MessageDataBuilder {
         self.html_parts.push_str("</strong>: ");
         self.html_parts.push_str(value);
 
-        self.break_character()
+        self.break_character();
     }
 
     /// Adds a key-value data pair to the message with a line break (or the default plain text separator).
@@ -74,10 +85,10 @@ impl MessageDataBuilder {
     /// # Examples
     /// Plain: `key: value`
     /// Rich: `<strong>key</strong>: <code>value</code>`
-    pub(crate) fn add_key_value_with_code(mut self, key: &str, value: &str) -> Self {
-        self.plain_parts.push(' ');
+    pub fn add_key_value_with_code(&mut self, key: &str, value: &str) {
+        self.plain_parts.push_str(" **");
         self.plain_parts.push_str(key);
-        self.plain_parts.push_str(": ");
+        self.plain_parts.push_str("**: ");
         self.plain_parts.push_str(value);
 
         self.html_parts.push_str("<strong>");
@@ -86,31 +97,68 @@ impl MessageDataBuilder {
         self.html_parts.push_str(value);
         self.html_parts.push_str("</code>");
 
-        self.break_character()
+        self.break_character();
     }
 
     /// Adds the data from some implementor of [MatrixMessageDataPart] to the end of the resulting [MessageData].
     /// This function _does not_ add any "break characters" after appending the [MatrixMessageDataPart] and
     /// requires that either the [MatrixMessageDataPart] supply the separators/line breaks or call [Self::break_character()].
-    pub(crate) fn add_matrix_message_part(mut self, part: impl MatrixMessageDataPart) -> Self {
+    pub fn add_matrix_message_part(&mut self, part: impl MatrixMessageDataPart) {
         self.plain_parts
             .push_str(&part.to_plain(DEFAULT_PLAIN_BREAK));
         self.html_parts.push_str(&part.to_html(DEFAULT_HTML_BREAK));
-        self
     }
 
     /// Adds a line of text followed by [Self::break_character()].
-    pub(crate) fn add_line(mut self, line: &str) -> Self {
+    pub fn add_line(&mut self, line: &str) {
         self.plain_parts.push_str(line);
-        self.html_parts.push_str(line);
-        self.break_character()
+        write!(self.html_parts, "<p>{}</p>", line).expect("Failed to write HTML String.");
+        self.break_character();
+    }
+
+    /// Adds a heading to the message. For HTML messages, this will be some `<h1>` through `<h6>` element surrounded by
+    /// a `<div>` with a trailing horizontal rule `<hr>`. For plain messages, this will be some text prefixed with a
+    /// Markdown header character and two line breaks.
+    pub fn add_heading(&mut self, heading: &SectionHeadingLevel, text: &str) {
+        let html_heading = match heading {
+            SectionHeadingLevel::One => "h1",
+            SectionHeadingLevel::Two => "h2",
+            SectionHeadingLevel::Three => "h3",
+            SectionHeadingLevel::Four => "h4",
+            SectionHeadingLevel::Five => "h5",
+            SectionHeadingLevel::Six => "h6",
+        };
+        write!(
+            self.html_parts,
+            "<div><{}>{}</{}></div><hr>",
+            html_heading, text, html_heading
+        )
+        .expect("Failed to write to underlying HTML String.");
+
+        let plain_heading = match heading {
+            SectionHeadingLevel::One => "#",
+            SectionHeadingLevel::Two => "##",
+            SectionHeadingLevel::Three => "###",
+            SectionHeadingLevel::Four => "####",
+            SectionHeadingLevel::Five => "#####",
+            SectionHeadingLevel::Six => "######",
+        };
+
+        write!(
+            self.plain_parts,
+            "{} {} {}{}",
+            plain_heading, text, DEFAULT_PLAIN_BREAK, DEFAULT_PLAIN_BREAK
+        )
+        .expect("Failed to write to underlying plain String.");
     }
 
     /// Copy the contents of this builder to a new [MessageData].
     ///
-    /// Note that for the [MessageData.plain], preceding and trailing whitespace is trimmed. For both [MessageData.plain]  
-    /// and [MessageData.html], trailing "break characters" (as inserted by [Self::break_character()]) are trimmed.
-    pub(crate) fn to_message_data(&self) -> MessageData {
+    /// Note that:
+    ///  * Preceding and trailing whitespace is trimmed.
+    ///  * Trailing "break characters" (as inserted by [Self::break_character()]) are trimmed.
+    ///  * For HTML messages only, a single "break character" is inserted after trimming for formatting.
+    pub fn to_message_data(&self) -> MessageData {
         MessageData::new(
             self.plain_parts
                 .trim_end_matches(DEFAULT_PLAIN_BREAK)
@@ -120,16 +168,25 @@ impl MessageDataBuilder {
     }
 }
 
+impl Default for MessageDataBuilder {
+    fn default() -> Self {
+        MessageDataBuilder::new()
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::message::message_data_builder::{MatrixMessageDataPart, MessageDataBuilder};
+    use crate::message::message_data_builder::{
+        MatrixMessageDataPart, MessageDataBuilder, SectionHeadingLevel,
+    };
 
     #[test]
     pub fn key_value_returns_result_without_trailing_break_character_given_single_use() {
         // Arrange
-        let expected_plain = "Test: Of KeyValue";
-        let expected_html = "<strong>Test</strong>: Of KeyValue";
-        let builder = MessageDataBuilder::new().add_key_value("Test", "Of KeyValue");
+        let expected_plain = "**Test**: Of KeyValue";
+        let expected_html = "<br><strong>Test</strong>: Of KeyValue";
+        let mut builder = MessageDataBuilder::new();
+        builder.add_key_value("Test", "Of KeyValue");
 
         // Act
         let actual = builder.to_message_data();
@@ -142,12 +199,12 @@ mod tests {
     #[test]
     pub fn key_value_returns_result_with_separating_break_character_given_multiple_uses() {
         // Arrange
-        let expected_plain = "Test: Of KeyValue | Test2: Of KeyValue2";
+        let expected_plain = "**Test**: Of KeyValue \n **Test2**: Of KeyValue2";
         let expected_html =
-            "<strong>Test</strong>: Of KeyValue <br><strong>Test2</strong>: Of KeyValue2";
-        let builder = MessageDataBuilder::new()
-            .add_key_value("Test", "Of KeyValue")
-            .add_key_value("Test2", "Of KeyValue2");
+            "<br><strong>Test</strong>: Of KeyValue <br><strong>Test2</strong>: Of KeyValue2";
+        let mut builder = MessageDataBuilder::new();
+        builder.add_key_value("Test", "Of KeyValue");
+        builder.add_key_value("Test2", "Of KeyValue2");
 
         // Act
         let actual = builder.to_message_data();
@@ -172,11 +229,28 @@ mod tests {
     #[test]
     pub fn add_matrix_message_part_inserts_as_is() {
         // Arrange
-        let expected_plain = "Testing test | 1: 2";
-        let expected_html = "<h1>Testing</h1><br>Test! <br><strong>1</strong>: 2";
-        let builder = MessageDataBuilder::new()
-            .add_matrix_message_part(TestMessageDataPart)
-            .add_key_value("1", "2");
+        let expected_plain = "Testing test \n **1**: 2";
+        let expected_html = "<br><h1>Testing</h1><br>Test! <br><strong>1</strong>: 2";
+        let mut builder = MessageDataBuilder::new();
+        builder.add_matrix_message_part(TestMessageDataPart);
+        builder.add_key_value("1", "2");
+
+        // Act
+        let actual = builder.to_message_data();
+
+        // Assert
+        assert_eq!(expected_plain, actual.plain);
+        assert_eq!(expected_html, actual.html);
+    }
+
+    #[test]
+    pub fn add_heading_inserts_expected() {
+        // Arrange
+        let expected_plain = "## Test 123 \n\n **1**: 2";
+        let expected_html = "<br><div><h2>Test 123</h2></div><hr><strong>1</strong>: 2";
+        let mut builder = MessageDataBuilder::new();
+        builder.add_heading(&SectionHeadingLevel::Two, "Test 123");
+        builder.add_key_value("1", "2");
 
         // Act
         let actual = builder.to_message_data();
--- crates/matrix_client/src/message/mod.rs
+++ crates/matrix_client/src/message/mod.rs
@@ -2,4 +2,4 @@ mod message_data;
 mod message_data_builder;
 
 pub use message_data::MessageData;
-pub use message_data_builder::{MatrixMessageDataPart, MessageDataBuilder};
+pub use message_data_builder::{MatrixMessageDataPart, MessageDataBuilder, SectionHeadingLevel};

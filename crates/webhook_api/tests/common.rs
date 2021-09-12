907af4cd3cef8b08fdfc3144c971e6e3af6fcc7c
Joel Dewey <hello@joeldewey.com>
2021-09-12 13:21:58 -0400

    Add tracing dependencies to tests.

--- crates/webhook_api/tests/common.rs
+++ crates/webhook_api/tests/common.rs
@@ -8,6 +8,11 @@ use yarrbot_db::models::MatrixRoom;
 use yarrbot_db::{build_pool, DbPool};
 use yarrbot_matrix_client::message::MessageData;
 use yarrbot_matrix_client::MatrixClient;
+use tracing_log::LogTracer;
+use tracing_subscriber::{EnvFilter, Registry};
+use yarrbot_common::environment::variables::LOG_FILTER;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::layer::SubscriberExt;
 
 static INIT: Once = Once::new();
 
@@ -22,6 +27,18 @@ pub fn setup() {
     INIT.call_once(|| {
         dotenv::from_filename("integrationtest.env").ok();
         initialize(&POOL);
+
+        // Copy and paste of what is in main.rs of the bin crate.
+        // Extracting this to a function in common.rs causes tracing to stop working.
+        LogTracer::init().expect("Could not initialize the LogTracer.");
+        let filter = EnvFilter::try_from_env(LOG_FILTER)
+            .unwrap_or_else(|_| EnvFilter::default())
+            .add_directive(LevelFilter::WARN.into());
+        let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stdout());
+        let subscriber = Registry::default()
+            .with(filter)
+            .with(tracing_subscriber::fmt::Layer::default().with_writer(non_blocking_writer));
+        tracing::subscriber::set_global_default(subscriber).expect("Could not set global subscriber for tracing.");
     });
 }
 
